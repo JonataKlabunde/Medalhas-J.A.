98949f1f8784ace08714eb789684ea8b6b4a7974
Jonata <jonata_k@yahoo.com.br>
2016-08-25 16:45:45 -0300

    inicio versão 2.0

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,28 +1,25 @@
 
-//** teste
-Alloy.Globals.efeitoClick($.viewOuroID, function(e){
-    Alloy.Globals.botaoClicado = 'ouro';
-    setTimeout(function(e){
-        Alloy.Globals.secoes.open();
-    },50);
-});
-
-Alloy.Globals.efeitoClick($.viewPrataID, function(e){
-    Alloy.Globals.botaoClicado = 'prata';
-    setTimeout(function(e){
-        Alloy.Globals.secoes.open();
-    },50);
-});
+$.barraWidget.setScrollableView($.scroll);
 
 
 
-//** Fechar o aplicativo.
-$.winIndexID.addEventListener('android:back', function(e) {
-    Alloy.Globals.currentActivity.finish();
-});
+
+var Admob = require('ti.admob');
+var adMobView = Admob.createView({ publisherId:"ca-app-pub-4602457716032817/6115395688", bottom: 0});
+// adMobView.addEventListener(Admob.AD_RECEIVED,function(){ Ti.API.info("ad received");});
+// adMobView.addEventListener(Admob.AD_NOT_RECEIVED,function(){ Ti.API.info("ad not received");});
+
+$.winIndexID.add(adMobView);
+function open(){
+}
+
+ 
+
+
+function viewSelecionada(e) {}
+function scrollAlterado(e) {}
+function voltar(e) {}
 
 
 
 $.winIndexID.open();
-
-
